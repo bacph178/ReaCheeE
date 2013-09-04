1af635dbdb4ffb135b61e4dc2ccac1f63a463559
Phung Hoang Bac <phung.hoang.bac@framgia.com>
2013-09-04 16:39:49 +0700

    Initial Commit

---
+++ ReacheeE/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  ReacheeEAppController.h
+//  ReacheeE
+//
+//  Created by macbook_016 on 2013/09/04.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
